78a3f784d61c310d40da0e50a5bdae0f2ef9624d
Ammon Johnson <ammonsawesome@gmail.com>
2021-04-16 10:05:47 -0700

    Added validateTxtInput() to validate users name

--- js/main.js
+++ js/main.js
@@ -3,5 +3,16 @@ window.onload = function () {
     submitbtn.onclick = main;
 };
 function main() {
-    alert("submitbtn was clicked");
+    validateTxtInput("first-name", "First name is required");
+    validateTxtInput("last-name", "Last name is required");
+}
+function validateTxtInput(id, errorMsg) {
+    var txtBox = document.getElementById(id);
+    var txtBoxValue = txtBox.value;
+    if (txtBoxValue == "") {
+        var errorSpan = txtBox.nextElementSibling;
+        errorSpan.innerText = errorMsg;
+        return false;
+    }
+    return true;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -4,5 +4,24 @@ window.onload = function() {
 }
 
 function main():void {
-    alert("submitbtn was clicked");
+    validateTxtInput("first-name", "First name is required");
+    validateTxtInput("last-name", "Last name is required");
+}
+
+/**
+ * 
+ * @param id id The id of the <input type="txt"> to validate
+ * @param errorMsg errMsg The message to display is the sibling span of the textbox
+ * @returns Returns true if the texbox with the given id has some text inside it
+ */
+function validateTxtInput(id:string, errorMsg:string):boolean {
+    let txtBox = <HTMLInputElement>document.getElementById(id);
+    let txtBoxValue = txtBox.value;
+
+    if (txtBoxValue == "") {
+        let errorSpan = <HTMLSpanElement>txtBox.nextElementSibling;
+        errorSpan.innerText = errorMsg;
+        return false;
+    }
+    return true;
 }
